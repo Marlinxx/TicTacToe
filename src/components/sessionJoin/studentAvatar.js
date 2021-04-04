cf55f14e9d5e3623fae677867b3206e99d9ca9c6
Nrmal Raj <nirmal.selvaraj@byjus.com>
2021-04-04 17:30:40 +0530

    remove unwanted commented lines

--- src/components/sessionJoin/studentAvatar.js
+++ src/components/sessionJoin/studentAvatar.js
@@ -14,12 +14,6 @@ export default function StudentAvatar({ student }) {
                 student.hasDp ? <img src={image.default} alt="logo" className="studentDp" /> : <div className="student">{student.avatarLetter}</div>
             }
       </div>
-      {/* <svg xmlns="http://www.w3.org/2000/svg">
-            <circle cx="50" cy="50" r="50" fill="red" />
-            {
-                student.hasDp ?  <img src={image.default} alt='logo' /> : <text fill="#ffffff" x="50" y="50"> {student.avatarLetter} </text>
-            }
-        </svg>    */}
     </Fragment>
   );
 }
